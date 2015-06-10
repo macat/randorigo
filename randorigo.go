bb752b7d25545620abcf93eb2935dd9180f89193
Attila Maczak <attila@maczak.hu>
2015-06-10 07:02:55 -0400

    progress at meetup

--- randorigo.go
+++ randorigo.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
 
+const MAX_VALUE int = 99999
+
 func main() {
-	fmt.Println("hello world")
 }
 
 func random() int {
 	time.Sleep(100 * time.Millisecond)
-	return rand.Intn(999999)
+	return rand.Intn(MAX_VALUE)
 }
 
 func randomSequence(length int) []int {
 	sequence := []int{}
+
 	randomChannel := make(chan int, 10)
 
 	for i := 0; i < length; i++ {
@@ -31,4 +32,19 @@ func randomSequence(length int) []int {
 	}
 
 	return sequence
+}
+
+func minFinder(begin int, end int, f func(int) int) int {
+	min := f(begin)
+	if begin > end {
+		begin, end = end, begin
+	}
+	for i := begin; i < end; i++ {
+		temp := f(i)
+		if temp < min {
+			min = temp
+		}
+	}
+
+	return min
 }
--- randorigo_test.go
+++ randorigo_test.go
@@ -1,38 +1,80 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+func getIdentity(i int) int { return i }
+func getInverse(i int) int  { return -i }
+func getInverseSlow(i int) int {
+	time.Sleep (100*time.Millisecond)
+	return -i
+}
 
 func TestRandomNumber(t *testing.T) {
 	number := random()
 	if number < 0 {
-		t.Error("random number should be greater than 0")
+		t.Error("Random number should be greater than 0")
 	}
-	if number > 999999 {
-		t.Error("random number should be smaller than 999999")
+
+	if number > 99999 {
+		t.Error("Random number should be less than 99999")
 	}
 }
 
-func TestRandomSequence(t *testing.T) {
+func TestRandomNumberSequence(t *testing.T) {
 	sequence := randomSequence(100)
-
 	if len(sequence) != 100 {
-		t.Error("sequence length is not corrent")
+		t.Error("sequence length should be 100")
 	}
 
-	sum := 0
-
-	for _, number := range sequence {
-		sum += number
-		if number < 0 {
-			t.Error("random number should be greater than 0")
-		}
-		if number > 999999 {
-			t.Error("random number should be smaller than 999999")
+	for _, n := range sequence {
+		if n < 0 || n > 999999 {
+			t.Error("number is not in range")
 		}
 	}
+}
 
-	avg := sum / 100
-	if avg == sequence[10] && avg == sequence[30] {
-		t.Error("sequence is not random")
+func TestSameInterval(t *testing.T) {
+	min := minFinder(1, 1, getIdentity)
+	if min != 1 {
+		t.Error("min should be 1")
+	}
+}
+
+func TestIncreasingInterval(t *testing.T) {
+	min := minFinder(10, 20, getIdentity)
+	if min != 10 {
+		t.Error("min should be 10")
+	}
+}
+
+func TestMaximumLessThanMinimum(t *testing.T) {
+	min := minFinder(20, 10, getIdentity)
+	if min != 10 {
+		t.Error("min should be 10")
+	}
+}
+
+func TestReturnsMinimumWithInverse(t *testing.T) {
+	min := minFinder(20, 10, getInverse)
+	if min != -20 {
+		t.Error("min should be -20")
+	}
+}
+
+func TestSquares(t *testing.T) {
+	min := minFinder(-1, 1, func(i int) int { return i * i })
+	if min != 0 {
+		t.Error("min should be 0")
+	}
+}
+
+func Test200msBasic(t *testing.T) {
+	startTime := time.Now()
+  minFinder(1, 10, getInverseSlow)
+  if time.Since(startTime) * time.Millisecond > 200 {
+		t.Error("time greater than 200ms")
 	}
 }
